ca3df5aae1388161911ca2492751c22016c13e09
PremMallela <premmallela381@gmail.com>
2025-05-06 12:43:31 +0530

    set secure to true to allow HTTPS sites to store cookies

--- Backend/handlers/authHandlers.js
+++ Backend/handlers/authHandlers.js
@@ -51,6 +51,7 @@ export const login = asyncHandler(async (req, res) => {
   res.cookie("token", token, {
       httpOnly: true,
       sameSite: "None",
+      secure: true,
       maxAge: 1 * 24 * 60 * 60 * 1000,
     })
     .status(200)
