9667b886cbd3c636190c6dc43d0e3457f56b23f5
PremMallela <premmallela381@gmail.com>
2025-05-12 23:20:19 +0530

    chore: retry CORS setup for Vercel ↔ Render integration

--- Backend/server.js
+++ Backend/server.js
@@ -15,9 +15,13 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL, 
+  "http://localhost:5173"         ];
+
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    origin: allowedOrigins 
   }));
 
 app.use(express.json());
